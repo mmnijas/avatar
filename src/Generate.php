81988155fe826327b73899924cf65ebed88981b7
Nijas M M <nijas.mm@iroidtechnologies.com>
2024-09-19 20:02:10 +0530

    initial commit

---
+++ src/Generate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mmnijas\Avatar;
+
+class Generate
+{
+    public static function image()
+    {
+        return "https://placehold.jp/50x50.png";
+    }
+
+    public static function letter($name)
+    {
+        return "https://placehold.jp/60x60.png";
+    }
+}
