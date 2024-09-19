b24165e05a11837cf0192fa33e96ba621db161d0
Nijas M M <nijas.mm@iroidtechnologies.com>
2024-09-19 20:57:04 +0530

    basic avatar function updated with intervention creation

--- src/Generate.php
+++ src/Generate.php
@@ -2,15 +2,93 @@
 
 namespace Mmnijas\Avatar;
 
+use Intervention\Image\ImageManagerStatic as Image;
+
 class Generate
 {
-    public static function image()
+    protected $name;
+    protected $size;
+    protected $backgroundColor;
+    protected $textColor;
+    protected $fontPath;
+
+    public function __construct($name, $size = 100, $backgroundColor = '#3498db', $textColor = '#ffffff')
     {
-        return "https://placehold.jp/50x50.png";
+        $this->name = $name;
+        $this->size = $size;
+        $this->backgroundColor = $backgroundColor;
+        $this->textColor = $textColor;
+        $this->fontPath = __DIR__ . '/fonts/OpenSans-Bold.ttf'; // Update with the actual path to your font
     }
 
-    public static function letter($name)
+    /**
+     * Generate Avatar.
+     *
+     * @param string $mode 'letter' for letter avatar, 'random' for random image
+     * @return \Intervention\Image\Image
+     */
+    public function generateAvatar($mode = 'letter')
     {
-        return "https://placehold.jp/60x60.png";
+        if ($mode === 'random') {
+            return $this->generateRandomImageAvatar();
+        } else {
+            return $this->generateLetterBasedAvatar();
+        }
+    }
+
+    /**
+     * Generate a letter-based avatar.
+     *
+     * @return \Intervention\Image\Image
+     */
+    protected function generateLetterBasedAvatar()
+    {
+        $initials = $this->getInitials($this->name);
+
+        // Create a blank canvas
+        $image = Image::canvas($this->size, $this->size, $this->backgroundColor);
+
+        // Add the initials as text
+        $image->text($initials, $this->size / 2, $this->size / 2, function ($font) {
+            $font->file($this->fontPath);
+            $font->size($this->size / 2);
+            $font->color($this->textColor);
+            $font->align('center');
+            $font->valign('middle');
+        });
+
+        return $image;
+    }
+
+    /**
+     * Generate a random image avatar.
+     *
+     * @return \Intervention\Image\Image
+     */
+    protected function generateRandomImageAvatar()
+    {
+        // You can use a free image API, or generate random patterns/colors
+        $randomImageUrl = "https://picsum.photos/{$this->size}";
+
+        // Download the random image
+        return Image::make($randomImageUrl);
+    }
+
+    /**
+     * Extract initials from a name.
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function getInitials($name)
+    {
+        $words = explode(' ', $name);
+        $initials = '';
+
+        foreach ($words as $word) {
+            $initials .= strtoupper($word[0]);
+        }
+
+        return $initials;
     }
 }
